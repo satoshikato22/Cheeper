e9db327640e85d3d858b059cae8212020b274bad
加藤怜 <jatgadm@gmail.com>
2019-12-30 18:51:11 +0900

    テスト

---
+++ Cheeper/src/main/java/com/example/demo/CheeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeperApplication.class, args);
+	}
+
+}
