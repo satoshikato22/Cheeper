4d5e6825f6a9827494c9b1e2731cba97145b4f21
加藤怜 <jatgadm@gmail.com>
2019-12-31 20:33:40 +0900

    ojdbc格納

--- Cheeper/src/main/java/com/example/demo/mutter/model/PostMutterLogic.java
+++ Cheeper/src/main/java/com/example/demo/mutter/model/PostMutterLogic.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class PostMutterLogic {
   public void execute(Mutter mutter, List<Mutter> mutterList) {
-    mutterList.add(0, mutter); // 先頭に追加 解説①
+    mutterList.add(0, mutter); // 先頭に追加
   }
 }
