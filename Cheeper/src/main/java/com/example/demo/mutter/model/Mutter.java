5cd2ab1df90d0b8091a651ef8627871f753e675e
加藤怜 <jatgadm@gmail.com>
2019-12-31 15:55:17 +0900

    テスト

--- Cheeper/src/main/java/com/example/demo/mutter/model/Mutter.java
+++ Cheeper/src/main/java/com/example/demo/mutter/model/Mutter.java
@@ -7,5 +7,11 @@ import lombok.Data;
 public class Mutter implements Serializable {
   private String userName; // ユーザー名
   private String text; // つぶやき内容
+  private int like; // よいねの数
+  private int dislike; // よくないねの数
 
+  public Mutter() {
+    like = 0;
+    dislike = 0;
+  }
 }
